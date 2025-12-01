fabff4480132805bc77a7e4dd698d99bba721f4f
Gutiiii <samuel.gutmans@gmail.com>
2025-12-01 10:29:26 +0100

    Person

---
+++ src/main/java/ch/prog1/wirlesennicht/data/Person.java
@@ -0,0 +1,26 @@
+package ch.prog1.wirlesennicht.data;
+
+import javax.print.attribute.standard.Media;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Person {
+    Map<String, Media> medias = new HashMap<>();
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Map<String, Media> getMedias() {
+        return medias;
+    }
+
+    public void addMedia(Media media) {
+        medias.put(media.getId(), media);
+    }
+}
