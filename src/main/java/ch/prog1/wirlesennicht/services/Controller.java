6850cc7973432b2584a0fb7579d95fd93146175a
iven <iven.kuder@bbzbl-it.ch>
2025-12-01 10:45:44 +0100

    controller "methods"

--- src/main/java/ch/prog1/wirlesennicht/services/Controller.java
+++ src/main/java/ch/prog1/wirlesennicht/services/Controller.java
@@ -1,7 +1,36 @@
 package ch.prog1.wirlesennicht.services;
 
+import ch.prog1.wirlesennicht.data.Media;
+import ch.prog1.wirlesennicht.data.Person;
+
+import java.util.List;
+
 public class Controller {
+
     public Controller() {
+
         //TODO: load sample media and create objects
+    }
+
+    public List<Media> getAllMedias(){
+        return null;
+    }
+
+    public Media getMediaById(int id){
+        return null;
+    }
+
+    public void lendMedia(Media media){
+
+    }
+    public List<Media> getMediasByPerson(String name){
+        return null;
+    }
+    public List<Person> getUsers(){
+        return null;
+    }
+
+    public void returnMedia(Media media){
+
     }
 }
